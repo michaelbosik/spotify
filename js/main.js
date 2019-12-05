6efd707536f3cb2a473ee3f72388346084ac9dce
MikeyBoss <michaelbosik@gmail.com>
2019-12-05 15:49:14 -0500

    sudo front end v1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-let access_token = "";
 
 function login(){
 
 }
 
 function getPlaylists(){
-
+    //hit server GET - returns list of names
+    //generateTable("User Lists", names)
 }
 
-function renderItem(){
+function plotData(data){
+    //chart data somehow Google Charts API?
+}
 
+function generateTable(id, list){
+    //iterate through list
+    //renderItem(id, item)
+}
+
+function sortList(id, params){
+    //hit server POST(id, params)
+    //onOK plotData(data)
+}
+
+function renderItem(listID, item){
+    //use listID to create row items
 }
